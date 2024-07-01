62de454d690c7990b719b4ac2950d0ec14b3cee5
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2024-07-01 15:22:13 +0530

    changes from michiel
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/lib/reactivity/reactive.js
+++ src/lib/reactivity/reactive.js
@@ -18,7 +18,7 @@
 import { track, trigger, pauseTracking, resumeTracking } from './effect.js'
 import symbols from '../symbols.js'
 
-const arrayPatchMethods = ['push', 'pop', 'shift', 'unshift', 'splice']
+const arrayPatchMethods = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort']
 
 const proxyMap = new WeakMap()
 
@@ -58,16 +58,20 @@ const reactiveProxy = (original, _parent = null, _key) => {
         // augment array path methods (that change the length of the array)
         if (arrayPatchMethods.indexOf(key) !== -1) {
           return function (...args) {
-            // pauseTracking()
+            pauseTracking()
             const result = target[key].apply(this, args)
+            resumeTracking()
             // trigger a change on the parent object and the key
             // i.e. when pushing a new item to `obj.data`, _parent will equal `obj`
             // and _key will equal `data`
-            // resumeTracking()
             trigger(_parent, _key)
             return result
           }
         }
+        if (key === 'length') {
+          return original.length
+        }
+
         return Reflect.get(target, key, receiver)
       }
 
